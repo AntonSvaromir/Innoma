ec70ecd8b551250fcddec0fd6ea03a47092fcc73
AntonSvaromir <130767595+AntonSvaromir@users.noreply.github.com>
2023-06-11 19:43:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var mixer = mixitup('.marketswork__mixitup');
+
+$('.marketswork__mixitup-btn').on('click', function () {
+   $('.marketswork__mixitup-btn').removeClass('marketswork__mixitup-btn--active')
+   $(this).addClass('marketswork__mixitup-btn--active')
+})
