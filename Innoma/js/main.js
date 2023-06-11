9318c7bbe0acfda534ed01f717fef132478d17cb
AntonSvaromir <130767595+AntonSvaromir@users.noreply.github.com>
2023-06-11 19:46:16 +0300

    Delete Innoma directory

--- Innoma/js/main.js
+++
@@ -1,6 +0,0 @@
-var mixer = mixitup('.marketswork__mixitup');
-
-$('.marketswork__mixitup-btn').on('click', function () {
-   $('.marketswork__mixitup-btn').removeClass('marketswork__mixitup-btn--active')
-   $(this).addClass('marketswork__mixitup-btn--active')
-})
